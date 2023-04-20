c97c3bd129f04e654cea2b51e3c97312a2c98d6b
Jenni <jenni.hynynen@esedulainen.fi>
2023-04-20 13:17:02 +0300

    korjattiin env copy

--- env-copy.php
+++ env-copy.php
@@ -1,6 +1,6 @@
 <?php
-define('EMAIL', 'mail@mail.com')
-define('DATABASE', 'jenni.hynynen.db')
-define('DB_USER'. 'root')
-define('DB_PASSWD'. '')
-
+define('EMAIL', 'mail@mail.com');
+define('DB_HOST', 'localhost');
+define('DATABASE', 'naytto');
+define('DB_USER', 'root');
+define('DB_PASSWD', '');
